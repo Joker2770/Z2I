327ff1253fbe073da84e6e6ac0f41a3d0d469dad
joker2770 <1214220480@qq.com>
2023-04-29 20:49:46 +0800

    fix: replace start with call. :bug:

--- scripts/train.bat
+++ scripts/train.bat
@@ -6,8 +6,8 @@ set i=0
 
 if %do_prepare%==1 (
     echo preparing........
-    train_net.bat prepare
-    python ..\\python\\learner.py
+    call train_net.bat prepare
+    call python ..\\python\\learner.py
 ) else (
     echo skip prepare!
 )
@@ -17,16 +17,16 @@ echo --------------%n%-th train------------------
 :INNER_LOOP
 if %i% LSS %batch_num% (
     echo generate data......
-    start /B /WAIT train_net.bat generate %i%
+    call train_net.bat generate %i%
     set /A i+=1
     goto INNER_LOOP
 )
-if %n% LEQ 10 (
+if %n% LEQ 2000 (
     echo --------------%n%-th train------------------
     set /A i=0
-    python ..\\python\\learner.py train
-    :: train_net.bat eval_with_winner 10
-    train_net.bat eval_with_random 10
+    call python ..\\python\\learner.py train
+    :: call train_net.bat eval_with_winner 10
+    call train_net.bat eval_with_random 10
     set /A n+=1
     goto INNER_LOOP
 )
